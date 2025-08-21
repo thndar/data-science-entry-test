2493f2f87802b6a9ff14514bf90a4eab54104921
thndar <thndarmm@gmail.com>
2025-08-21 15:22:19 +0800

    Add files via upload

---
+++ q4.py
@@ -0,0 +1,20 @@
+#def string_reverse(s):
+#    """
+#    Task 1
+#    - Create a function that reverses a given string (s).
+#    - s must be a string.
+#    - Return the reversed string.
+#    """
+#   return
+
+def string_reverse(s):
+    print(s)
+    print(s[::-1])
+
+# Task 2
+# Invoke the function "string_reverse" using the following scenarios:
+# - "Hello World"
+# - "Python"
+
+string_reverse("Hello World")
+string_reverse("Python")
